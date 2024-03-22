5e558b599f2b3bdf5b50451e90d7a04fca134c17
lorenzo-de-giorgi <lorenzodegiorgi2004@gmail.com>
2024-03-22 13:35:32 +0100

    first snack completed

---
+++ js/script.js
@@ -0,0 +1,62 @@
+// snack 1
+let list = [
+    {
+        'tableName': 'Tavolo Vip',
+        'guestName': 'Brad Pitt',
+        'place': 1
+    },
+    {
+        'tableName': 'Tavolo Vip',
+        'guestName': 'Johnny Depp',
+        'place': 2
+    },
+    {
+        'tableName': 'Tavolo Vip',
+        'guestName': 'Lady Gaga',
+        'place': 3
+    },
+    {
+        'tableName': 'Tavolo Vip',
+        'guestName': 'Cristiano Ronaldo',
+        'place': 4
+    },
+    {
+        'tableName': 'Tavolo Vip',
+        'guestName': 'Georgina Rodriguez',
+        'place': 5
+    },
+    {
+        'tableName': 'Tavolo Vip',
+        'guestName': 'Chiara Ferragni',
+        'place': 6
+    },
+    {
+        'tableName': 'Tavolo Vip',
+        'guestName': 'George Clooney',
+        'place': 7
+    },
+    {
+        'tableName': 'Tavolo Vip',
+        'guestName': 'Amal Clooney',
+        'place': 8
+    },
+    {
+        'tableName': 'Tavolo Vip',
+        'guestName': 'Fedez',
+        'place': 9
+    },
+    {
+        'tableName': 'Tavolo Vip',
+        'guestName': 'Amadeus',
+        'place': 10
+    },
+    {
+        'tableName': 'Tavolo Vip',
+        'guestName': 'Fiorello',
+        'place': 11
+    },
+];
+
+console.log(list);
+
+// snack 2
